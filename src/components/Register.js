f8971a8a6ccd14106aea78d273dea05db28ff23f
Marek Chmielewski <Metron45@gmail.com>
2019-04-13 13:38:57 +0200

    Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
+import "./Register.css";
 
 export class Register extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.email)
+  }
+
   render() {
     return (
+      <div className="Register">
       <div>
-        
+        <h3>Registration Form</h3>
       </div>
+      <form onSubmit={this.handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <Form.Label>Email</Form.Label>
+          <FormControl
+            autoFocus
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <Form.Label>Password</Form.Label>
+          <FormControl
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password"
+          />
+        </FormGroup>
+        <FormGroup controlId="repeat_password" bsSize="large">
+          <Form.Label>Repeat Password</Form.Label>
+          <FormControl
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password"
+          />
+        </FormGroup>
+        <Button
+          block
+          bsSize="large"
+          disabled={!this.validateForm()}
+          type="submit"
+        >
+          Register
+        </Button>
+      </form>
+    </div>
     )
   }
 }
